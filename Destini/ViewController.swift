4e29c641b2b498f7f45b6e7bd8a61d345c5579d2
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-01-24 14:39:00 -0300

    aula 10

---
+++ Destini/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Paola Alcantara Soares on 24/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+     
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+}
+
+
